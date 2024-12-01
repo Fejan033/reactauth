d9344a669d529c3ce778eb43f59136f554a1ec29
Mohammed <fejan033@gmail.com>
2024-12-01 17:45:33 +0530

    ProjectInitiatedWithFirebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0jOX6H4PUuKwLyWn3iro_79VgGoIia2s",
+  authDomain: "techpractice-cf150.firebaseapp.com",
+  projectId: "techpractice-cf150",
+  storageBucket: "techpractice-cf150.firebasestorage.app",
+  messagingSenderId: "735948629336",
+  appId: "1:735948629336:web:e8fe29c1b0e3959233f12d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
